6298edd6636afd9cc4a92957c05710b5090db01d
Cristhiam Vasquez <cristhiam.vasquez@gmail.com>
2020-08-26 15:53:28 -0400

    Aplicacion funcionando - login mediante Auth0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { AuthGuard } from './services/auth.guard';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  { path: 'protegida', component: ProtegidaComponent, canActivate: [ AuthGuard ] },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
